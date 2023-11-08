2506a20d2f25a58d9f6bc4b2f3a968592c077d4e
Rodion Sabodash <rodion.sabodash@raiffeisen.ua>
2023-11-08 17:36:45 +0200

    feature/hw12: Done

--- main.go
+++ main.go
@@ -1,10 +1,59 @@
-package golang_structs
+package main
 
-import "regexp"
+import (
+	"flag"
+	"log"
+	"os"
+	"regexp"
+)
 
-const numRegex = "\\(?(\\+?\\d{1,3})?\\)?[\\s.-]?(\\d{3})[\\s.-]?(\\d{3})[\\s.-]?(\\d{4})\n"
+const numRegex = "\\(?\\d{3}\\)?[-.\\s]?\\d{3}[-.\\s]?\\d{4}"
+const wordRegex = "Лес[яії]?\\s+Україн[а-яіїє]*"
 
-func numSearch(text string) int {
-	r, _ := regexp.Compile(numRegex)
-	return 1
+// numSearch searches for any form of phone number in given text
+func numSearch(filename string) {
+	r, err := regexp.Compile(numRegex)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileData, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	numbers := r.FindAllString(string(fileData), -1)
+	for _, number := range numbers {
+		println("Match:", number)
+	}
+}
+
+// wordSearch searches for any form of "Леся Українка" in given text
+func wordSearch(filename string) {
+	r, err := regexp.Compile(wordRegex)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileData, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	numbers := r.FindAllString(string(fileData), -1)
+	for _, text := range numbers {
+		println("Match text:", text)
+	}
+}
+
+func main() {
+	nums := flag.Bool("nums", false, "Display phone numbers in a file")
+	words := flag.Bool("words", false, "search for \"Леся Українка\" in a file")
+	filename := flag.String("file", "default.txt", "file to search")
+
+	flag.Parse()
+
+	if *nums {
+		numSearch(*filename)
+	} else if *words {
+		wordSearch(*filename)
+	} else {
+		log.Println("No valid search option selected.")
+	}
 }
